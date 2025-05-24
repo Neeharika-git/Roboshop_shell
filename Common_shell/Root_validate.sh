48328298402c784a6d7dc181f4f878262c5149e9
neeharika <neeharikanew@gmail.com>
2025-05-24 06:48:18 +0530

    Root

---
+++ Common_shell/Root_validate.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+USER_ID = $0
+
+if(USER_ID ne 0)
+    
+    {
+        echo "please login with root user"
+    }
+else
+    {
+        Echo "You are in root user, proceeding further"
+    }
+fi
