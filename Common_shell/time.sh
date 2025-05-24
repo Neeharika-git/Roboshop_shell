0e6516ed0ab3ab02015296cdf40eb54a9384f438
neeharika <neeharikanew@gmail.com>
2025-05-24 07:47:50 +0530

    manual

--- Common_shell/time.sh
+++ Common_shell/time.sh
@@ -3,5 +3,6 @@
 start_time=$(date +%s)
 start_date=$(date +%d)
 start_month=$(date +%m)
+start_day=$(date +%f)
 
-echo $start_time $start_date $start_month
+echo $start_time $start_date $start_month $start_day
